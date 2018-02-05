330df2762601cdab5c0790269ad60a95ce63beae
MI\mpace <mikepace@quickenloans.com>
2018-02-05 10:54:59 -0500

    Initial .NET commit

---
+++ DotNetServerless/DotNetServerless/StepFunctionTasks.cs
@@ -0,0 +1,38 @@
+using Amazon.Lambda.Core;
+
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace DotNetServerless
+{
+    public class StepFunctionTasks
+    {
+        public State Greeting(State state, ILambdaContext context)
+        {
+            state.Message = "Hello";
+
+            if(!string.IsNullOrEmpty(state.Name))
+            {
+                state.Message += " " + state.Name;
+            }
+
+            // Tell Step Function to wait 5 seconds before calling 
+            state.WaitInSeconds = 5;
+
+            return state;
+        }
+
+        public State Salutations(State state, ILambdaContext context)
+        {
+            state.Message += ", Goodbye";
+
+            if (!string.IsNullOrEmpty(state.Name))
+            {
+                state.Message += " " + state.Name;
+            }
+
+            return state;
+        }
+    }
+}
